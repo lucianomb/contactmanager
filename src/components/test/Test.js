da9f59671fb2d86f99281aa418add60c57e5b801
Luciano M. Baldicerotti <luciano.xc@gmail.com>
2019-05-19 12:55:53 -0300

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  // componentWillMount() {
+  //   console.log("component will mount...");
+  // }
+
+  // componentDidUpdate() {
+  //   console.log("component did update...");
+  // }
+
+  // componentWillUpdate() {
+  //   console.log("component will update...");
+  // }
+
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log("component will receive props...");
+  // }
+
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   return {
+  //     test: "something"
+  //   };
+  // }
+
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log("get snapshot before update...");
+  // }
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
