4a5cb8c540bae1224786b727d518282cffb03788
Luciano M. Baldicerotti <luciano.xc@gmail.com>
2019-05-19 13:02:04 -0300

    Updates

---
+++ precache-manifest.8498f61a0fce173efffac69d7b95d277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416e1820453d7be0bb690778ac24ed7a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "36113ffdfd305ca8d8a3",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "36113ffdfd305ca8d8a3",
+    "url": "/contactmanager/static/js/main.95b4300d.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
